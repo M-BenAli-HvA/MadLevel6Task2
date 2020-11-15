556b5eda65dfdad28747fe9f50899508eb37767e
M-BenAli-HvA <mohamed.ben.ali@hva.nl>
2020-11-15 16:44:19 +0100

    Change submit button color and hide API_KEY

--- app/src/main/java/com/example/madlevel6task2/services/MoviesApiService.kt
+++ app/src/main/java/com/example/madlevel6task2/services/MoviesApiService.kt
@@ -1,12 +1,15 @@
 package com.example.madlevel6task2.services
 
+import com.example.madlevel6task2.BuildConfig
 import com.example.madlevel6task2.models.MoviesList
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MoviesApiService {
 
-   @GET("discover/movie?api_key=1aedca454e9de96637badb619d147f7c&language=en-US")
+
+
+   @GET("discover/movie?api_key=${BuildConfig.API_KEY}&language=en-US")
    suspend fun getMoviesByYear(@Query("year") year: Int,
                                 @Query("sort") sort: String? = "popularity.desc")
     : MoviesList
